7d2f7bdd1a218105778a5e5bc979c725c5f0e5e9
antoineschmitt <as@gratin.org>
2026-02-06 15:51:32 +0100

    test blocking

--- src/la_machine_definitions.hrl
+++ src/la_machine_definitions.hrl
@@ -178,8 +178,8 @@
 
 %% DEBUG
 % play only one mood (to debug)
--define(DEBUG_PLAY_ONLY_ONE_MOOD, 0).
+-define(DEBUG_PLAY_ONLY_ONE_MOOD, 1).
 % which mood
--define(DEBUG_PLAY_ONLY_ONE_MOOD_MOOD, excited).
+-define(DEBUG_PLAY_ONLY_ONE_MOOD_MOOD, test).
 % which scenario number (starting at 1, -1 means random)
--define(DEBUG_PLAY_ONLY_ONE_MOOD_INDEX, -1).
+-define(DEBUG_PLAY_ONLY_ONE_MOOD_INDEX, 1).
