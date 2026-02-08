2d2fa95edb62bc7a8c00c9d97e351cb26e00d023
Paul Guyot <pguyot@kallisys.net>
2026-02-08 14:42:13 +0100

    Enlarge avm partition to 1 MB
    Signed-off-by: Paul Guyot <pguyot@kallisys.net>

--- snds_upload.sh
+++ snds_upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-~/Library/Arduino15/packages/esp32/tools/esptool_py/5.1.0/esptool --chip esp32c3 --port /dev/cu.usbmodem101 write_flash 0x180000 _build/generated/sounds.bin
+~/Library/Arduino15/packages/esp32/tools/esptool_py/5.1.0/esptool --chip esp32c3 --port /dev/cu.usbmodem101 write_flash 0x230000 _build/generated/sounds.bin
